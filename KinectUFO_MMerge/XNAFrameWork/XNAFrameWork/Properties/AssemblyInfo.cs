9d6ba012d4652352c76c79b31948bdb99b4432f8
BrandonGohNianZhou <brandon0529@hotmail.com>
2014-03-19 17:00:23 +0800

    Properties

---
+++ KinectUFO_MMerge/XNAFrameWork/XNAFrameWork/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する全般的な情報は、以下の一連の属性によって管理されます。 
+//  General information about the assembly, are managed by a set of attributes of the following.
+// アセンブリに関連付けられている情報を変更するには、これらの属性値を変更します。
+//  You can change the information that is associated with the assembly, you can change the values ​​of these attributes.
+// 
+[assembly: AssemblyTitle("XNAFrameWork")]
+[assembly: AssemblyProduct("XNAFrameWork")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible 属性を False に設定すると、このアセンブリに含まれる型が 
+//  If set to False the ComVisible attribute, the type that is included in this assembly
+// COM コンポーネントに公開されません。COM からこのアセンブリの型にアクセスする必要がある場合は、 
+//  Will not be exposed to COM components. If you need to access a type in this assembly to COM,
+// その型の ComVisible 属性を True に設定してください。
+//  Please set to True the ComVisible attribute of that type.
+// Windows アセンブリだけが COM に対応しています。
+//  Windows only assembly supports COM.
+//  
+[assembly: ComVisible(false)]
+
+// Windows 上では、このプロジェクトが COM に公開されている場合、 次の GUID が typelib の ID として設定されます。
+//  On Windows, if this project is exposed to COM, GUID of the following will be set as the ID of the typelib.
+// Windows 以外のプラットフォームでは、このアセンブリをデバイスに展開するときに
+//  On platforms other than Windows, then when you want to deploy to the device this assembly
+// タイトル ストレージ コンテナーを一意に識別するために使用します。
+//  It is used to uniquely identify the title storage container.
+//  
+[assembly: Guid("d5b82589-95ed-4639-a08a-ac23cf53ab7e")]
+
+// アセンブリのバージョン情報は、次の 4 つの値で構成されています。
+//  Version information for an assembly, are made up of the following four values:.
+//
+//      メジャー バージョン  Major version
+//      マイナー バージョン  Minor version
+//      ビルド番号           Build Number
+//      リビジョン番号         Revision number
+//
+[assembly: AssemblyVersion("1.0.0.0")]
